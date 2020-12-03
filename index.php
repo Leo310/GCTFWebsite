baf73bde7f954eb073537383028730b8ddb75588
Leo <leonardheininger1@gmail.com>
2020-12-03 18:20:30 +0100

    + style.css (stylesheet)

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
     <head>
         <meta charset="utf-8">
         <title>MGOD</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <h1>
-            Setup
-        </h1>
+        <p>
+        I'm baby pabst blog brooklyn, af gastropub put a bird on it hell of tilde. Poutine XOXO +1,
+         cred affogato squid taiyaki la croix gentrify plaid palo santo hashtag direct trade ugh. 
+         Meh forage man braid pickled subway tile crucifix, thundercats iPhone taiyaki XOXO deep v slow-carb 
+         iceland banh mi jianbing. La croix kickstarter hell of godard narwhal 90's. Shabby chic marfa mlkshk food truck, 
+         copper mug poutine austin kinfolk kombucha. Shaman ramps cray succulents freegan, quinoa deep v you probably 
+         haven't heard of them. Schlitz affogato snackwave, vaporware keffiyeh typewriter celiac.
+        </p>
     </body>
 </html>

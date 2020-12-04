88b86ac1fc89fda05e8bb33c4a56b233a0a718b6
Leo <leonardheininger1@gmail.com>
2020-12-04 23:05:40 +0100

    + Basic file structure, Fonts

--- index.php
+++ index.php
@@ -3,16 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>MGOD</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
     </head>
     <body>
-        <p>
-        I'm baby pabst blog brooklyn, af gastropub put a bird on it hell of tilde. Poutine XOXO +1,
-         cred affogato squid taiyaki la croix gentrify plaid palo santo hashtag direct trade ugh. 
-         Meh forage man braid pickled subway tile crucifix, thundercats iPhone taiyaki XOXO deep v slow-carb 
-         iceland banh mi jianbing. La croix kickstarter hell of godard narwhal 90's. Shabby chic marfa mlkshk food truck, 
-         copper mug poutine austin kinfolk kombucha. Shaman ramps cray succulents freegan, quinoa deep v you probably 
-         haven't heard of them. Schlitz affogato snackwave, vaporware keffiyeh typewriter celiac.
-        </p>
+        <h1>Basics setup</h1>
+        <p>File structure and Teewordls font</p>
     </body>
 </html>

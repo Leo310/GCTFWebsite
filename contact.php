367c4c621131d641e1f9357cfa7a8dd52ffdc426
Leo <leonardheininger1@gmail.com>
2020-12-09 23:17:31 +0100

    + better Navigationbar (for mobiles)

--- contact.php
+++ contact.php
@@ -1,22 +1,62 @@
 <!DOCTYPE html>
 <html lang="de">
-    <head>
-        <meta charset="utf-8">
-        <title>MGOD</title>
-        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
-    </head>
-    <body>
-        <nav class="header-nav">
-            <a href="index.php"><img src="resources/img/TwIcon.png" alt="Home"></a>
 
-            <ul>
-                <li><a href="index.php">Home</a></li
-                ><li><a href="history.php">History</a></li
-                ><li><a href="aboutus.php">About us</a></li
-                ><li class="active"><a href="contact.php">contact</a></li>
-            </ul>
-        </nav>
-        <h1>contact</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum, tellus eget pellentesque ullamcorper, metus dolor semper massa, eget interdum augue diam eu dui. Quisque sapien sem, posuere vel rutrum eget, auctor ac nulla. Vestibulum laoreet gravida tortor, sed pulvinar diam ultrices et. Sed id luctus nulla. Suspendisse quis massa ut eros ornare hendrerit nec a lacus. Sed et elit in neque tincidunt volutpat. Sed tristique ultricies arcu, id viverra magna venenatis id. Curabitur quis rutrum massa. Nullam tristique, turpis quis pulvinar aliquam, ex purus eleifend dui, et semper tellus risus sit amet dui. Proin tristique in justo vel gravida.</p>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MGOD</title>
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="header-nav">
+
+        <div class="desktop-nav container">
+
+            <div class="row">
+                <div class="col-6">
+                    <a href="index.php"><img src="resources/img/TwIcon.png" alt="Home"></a>
+
+                    <ul>
+                        <li><a href="index.php">Home</a></li
+                        ><li><a href="history.php">History</a></li
+                        ><li><a href="aboutus.php">About us</a></li
+                        ><li class="active"><a href="contact.php">contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="small-nav container">
+
+            <div class="row">
+                <div class="col-6">
+                    <a href="index.php"><img src="resources/img/TwIcon.png" alt="Home"></a>
+                    <div class="mobile-nav clearfix">
+                        <div class="mobile-nav-button"><span>&equiv;</span></div>
+                        <div class="mobile-nav-content clearfix">
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="history.php">History</a></li>
+                                <li><a href="aboutus.php">About us</a></li>
+                                <li class="active"><a href="contact.php">contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <header class="banner-header"></header>
+    <h1>contact</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum, tellus eget pellentesque
+        ullamcorper, metus dolor semper massa, eget interdum augue diam eu dui. Quisque sapien sem, posuere vel rutrum
+        eget, auctor ac nulla. Vestibulum laoreet gravida tortor, sed pulvinar diam ultrices et. Sed id luctus nulla.
+        Suspendisse quis massa ut eros ornare hendrerit nec a lacus. Sed et elit in neque tincidunt volutpat. Sed
+        tristique ultricies arcu, id viverra magna venenatis id. Curabitur quis rutrum massa. Nullam tristique, turpis
+        quis pulvinar aliquam, ex purus eleifend dui, et semper tellus risus sit amet dui. Proin tristique in justo vel
+        gravida.</p>
+</body>
+
 </html>
